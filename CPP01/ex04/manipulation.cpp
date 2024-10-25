2b0f9b3fd047ec33ae46483a7178a8f29e63cdc3
Ancolen <114772079+Ancolen@users.noreply.github.com>
2024-10-25 13:02:19 +0300

    Update manipulation.cpp

--- CPP01/ex04/manipulation.cpp
+++ CPP01/ex04/manipulation.cpp
@@ -2,12 +2,12 @@
 #include <cstdlib>
 manipulation::manipulation(string fileName, string s1, string s2) : fileName(fileName), s1(s1), s2(s2)
 {
-    __f.open(fileName.c_str());
-    if(!__f.is_open())
-    {
-        std::cerr << "file cant open" << std::endl;
-        std::exit(0);
-    }
+    //__f.open(fileName.c_str());
+    //if(!__f.is_open())
+    //{
+    //    std::cerr << "file cant open" << std::endl;
+    //    std::exit(0);
+    //}
 }
 
 void manipulation::replaceFile()
